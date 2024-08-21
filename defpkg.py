38a9a500cc66434d9ef75bc8612635beb3c4b2d2
risknu <145287544+risknu@users.noreply.github.com>
2024-08-21 19:01:16 +0200

    Added EU & palitra
    Added TikTok meme - palitra(LGBT meme) and EU - EUROPAPA!!

--- defpkg.py
+++ defpkg.py
@@ -3,6 +3,24 @@ from __future__ import annotations
 import functools
 from typing import Callable, Any
 
+def eu(func: Callable[..., Any], /) -> Callable[..., Any]:
+    "eu?"
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs) -> Any:
+        print("\33[1m\33[34meu?\33[0m")
+        return func(*args, **kwargs)
+    return wrapper
+
+def palitra(func: Callable[..., Any], /) -> Callable[..., Any]:
+    """rainbow."""
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs) -> Any:
+        print("".join([
+                f"\33[{i}m {i} \33[0m" for i in range(119)
+            ])+'\33[0m', sep='', end='\n')
+        return func(*args, **kwargs)
+    return wrapper
+
 def whatasigma(func: Callable[..., Any], /) -> Callable[..., Any]:
     """Ummm, what's the sigma function?"""
     @functools.wraps(func)
@@ -11,16 +29,17 @@ def whatasigma(func: Callable[..., Any], /) -> Callable[..., Any]:
         return func(*args, **kwargs)
     return wrapper
 
-# TODO: remake `class` from zero, optimizate
 def stasikmethod(func: Callable[..., Any], /) -> Callable[..., Any]:
     """Ummmmmm, Stasik method..."""
     @staticmethod
+    @functools.wraps(func)
     def wrapper(*args, **kwargs) -> Any:
         return func(*args, **kwargs)
     return wrapper
 
 def commit(func: Callable[..., Any], /) -> Callable[..., Any]:
     """Commit this part of the code, dude."""
+    @functools.wraps(func)
     def wrapper(*args, **kwargs) -> Any:
         print('\33[1m\33[35mdid you commit?', '\33[3m\33[90m(edited)', sep='\33[0m', end='\33[0m\n')
         return func(*args, **kwargs)
@@ -28,6 +47,7 @@ def commit(func: Callable[..., Any], /) -> Callable[..., Any]:
 
 def debug_stick(func: Callable[..., Any], /) -> Callable[..., Any]:
     """If it looks like a duck, swims like a duck, and quacks like a duck, it might be... a string in Python."""
+    @functools.wraps(func)
     def wrapper(*args, **kwargs) -> Any:
         function_result: Any = func(*args, **kwargs)
         if function_result is not None:
